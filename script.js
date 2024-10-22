7528a2bf6c9d12d1125a83d90d2ce107b350dfda
e-candeloro <candeloroettore@gmail.com>
2024-10-22 11:50:03 +0200

    added animations

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 // Initialize AOS
 AOS.init();
-
-// Heart button event listener
 const button = document.querySelector(".heart-secret-button");
+const blocks = document.querySelectorAll(".block");
+// Select all elements to observe
+const elementsToObserve = document.querySelectorAll(
+  ".card, .cv-card, .contact-card, .nav-arrow, .reverse, .heart-secret-button, .block, .nav-link, .link, .button-primary, .button-secondary"
+);
 
+let previousBlock = null;
+
+// Throttle function to optimize scroll event
+let isThrottled = false;
+
+function highlightRandomBlock() {
+  // Remove .active class from previous block
+  if (previousBlock) {
+    previousBlock.classList.remove("active");
+  }
+
+  // Randomly select a block
+  const randomIndex = Math.floor(Math.random() * blocks.length);
+  const currentBlock = blocks[randomIndex];
+
+  // Add .active class to the selected block
+  currentBlock.classList.add("active");
+
+  // Update the previous block
+  previousBlock = currentBlock;
+}
+
+// Initial call to highlight a block
+highlightRandomBlock();
+
+setInterval(highlightRandomBlock, Math.random() * 2000 + 1000); // Random interval
 button.addEventListener("click", function () {
   party.confetti(this, {
     count: party.variation.range(20, 30),
@@ -19,11 +48,6 @@ button.addEventListener("click", function () {
     ],
   });
 });
-
-// Select all elements to observe
-const elementsToObserve = document.querySelectorAll(
-  ".card, .cv-card, .contact-card, .nav-arrow, .reverse, .heart-secret-button, .block, .nav-link, .link, .button-primary, .button-secondary"
-);
 
 // Function to check elements in view
 function checkElementsInView() {
@@ -46,9 +70,6 @@ function checkElementsInView() {
     }
   });
 }
-
-// Throttle function to optimize scroll event
-let isThrottled = false;
 
 function throttle(fn, wait) {
   return function () {
